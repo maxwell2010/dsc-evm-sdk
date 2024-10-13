0cddedc9fe04bee15bc70fbc5d6c111cb6a609ce
Maxim <maxwell2010@yandex.ru>
2024-10-13 20:02:28 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+import os
+import subprocess
+import sys
+
+def install_node():
+    if sys.platform.startswith('linux'):
+        subprocess.check_call(['sudo', 'apt', 'install', '-y', 'nodejs', 'npm'])
+    elif sys.platform == 'darwin':
+        print("Установите Node.js вручную для macOS.")
+    elif sys.platform.startswith('win'):
+        print("Установите Node.js вручную для Windows.")
+    else:
+        print("Неизвестная операционная система. Установите Node.js вручную.")
+
+def clone_repositories():
+    # Клонирование репозитория с JavaScript SDK
+    subprocess.check_call(['git', 'clone', 'https://github.com/maxwell2010/dsc-evm-sdk.git'])
+
+    # Клонирование репозитория
+    subprocess.check_call(['git', 'clone', '--branch', 'master', 'https://bitbucket.org/decimalteam/dsc-js-sdk.git'])
+    os.chdir('dsc-js-sdk')
+
+def install_javascript_dependencies():
+    subprocess.check_call(['npm', 'install'])
+
+def install_python_dependencies():
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'py_mini_racer'])
+
+def main():
+    clone_repositories()
+    install_node()
+    install_javascript_dependencies()
+    os.chdir('..')  # Возврат в предыдущую папку
+    install_python_dependencies()
+    print("Установка завершена!")
+
+if __name__ == '__main__':
+    main()
