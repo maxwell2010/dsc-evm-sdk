a93d5ca7e3ce94f1d5bb24b0b1d89150b8208edd
Maxim <maxwell2010@yandex.ru>
2024-10-16 20:46:06 +0700

    Update dsc_python.py

--- dsc_python.py
+++ dsc_python.py
@@ -2,9 +2,20 @@ import asyncio
 import json
 import subprocess
 import os
-import logging
+from web3 import Web3
 
-logging.basicConfig(level=logging.INFO)
+
+class Helpers:
+    def __init__(self):
+        """
+        Инициализация вспомогательных функций.
+        """
+
+    async def bigInt(self, value: str):
+        try:
+            return int(value, 18)
+        except:
+            return 0
 
 
 class DecimalSDK:
@@ -40,16 +51,16 @@ class DecimalSDK:
             return {"success": False, "data": "Invalid JSON response"}
 
     async def token_by_symbol(self, symbol: str) -> dict:
-        """
-        Получение адреса токена по символу.
+        """ +
+        Получение контракт адреса токена по символу.
 
-        :param symbol: Символ токена.
+        :param symbol: Тикер токена.
         :return: Результат выполнения функции.
         """
         return await self.call_js_function('TokenBySymbol', symbol.lower())
 
     async def send_del(self, to: str, amount: float, mnemonic: str) -> dict:
-        """
+        """ +
         Отправка DEL токенов.
 
         :param to: Адрес получателя.
@@ -60,7 +71,7 @@ class DecimalSDK:
         return await self.call_js_function('sendDEL', to, str(amount), mnemonic)
 
     async def burn_del(self, amount: float, mnemonic: str) -> dict:
-        """
+        """ +
         Сжигание DEL токенов.
 
         :param amount: Сумма для сжигания.
@@ -69,8 +80,17 @@ class DecimalSDK:
         """
         return await self.call_js_function('burnDEL', str(amount), mnemonic)
 
-    async def transfer_token(self, token_address: str, to: str, amount: float, mnemonic: str) -> dict:
+    async def balance_del(self, address: str) -> dict:
+        """ +
+        Баланс DEL в 10^18.
+
+        :param address: EVM кошелек.
+        :return: Результат выполнения функции.
         """
+        return await self.call_js_function('balanceDEL', str(address))
+
+    async def transfer_token(self, token_address: str, to: str, amount: float, mnemonic: str) -> dict:
+        """ +
         Перевод токенов.
 
         :param token_address: Адрес токена.
@@ -94,8 +114,20 @@ class DecimalSDK:
         """
         return await self.call_js_function('TransferFromToken', token_address, from_address, to, str(amount), mnemonic)
 
-    async def burn_token(self, token_address: str, amount: float, mnemonic: str) -> dict:
+    async def transfer_token_symbol(self, symbol: str, to: str, amount: float, mnemonic: str) -> dict:
+        """ +
+        Перевод токенов.
+
+        :param symbol: Тикер токена.
+        :param to: Адрес получателя.
+        :param amount: Сумма для перевода.
+        :param mnemonic: Мнемоническая фраза.
+        :return: Результат выполнения функции.
         """
+        return await self.call_js_function('TransferTokenSymbol', symbol, to, str(amount), mnemonic)
+
+    async def burn_token(self, token_address: str, amount: float, mnemonic: str) -> dict:
+        """ +
         Сжигание токенов.
 
         :param token_address: Адрес токена.
@@ -104,6 +136,17 @@ class DecimalSDK:
         :return: Результат выполнения функции.
         """
         return await self.call_js_function('BurnToken', token_address, str(amount), mnemonic)
+
+    async def burn_token_symbol(self, symbol: str, amount: float, mnemonic: str) -> dict:
+        """ +
+        Сжигание токенов.
+
+        :param symbol: Тикер токена.
+        :param amount: Сумма для сжигания.
+        :param mnemonic: Мнемоническая фраза.
+        :return: Результат выполнения функции.
+        """
+        return await self.call_js_function('BurnTokenSymbol', symbol, str(amount), mnemonic)
 
     async def approve_token(self, token_address: str, spender: str, amount: float, mnemonic: str) -> dict:
         """
@@ -118,7 +161,7 @@ class DecimalSDK:
         return await self.call_js_function('ApproveToken', token_address, spender, str(amount), mnemonic)
 
     async def buy_del_for_token(self, token_address: str, amount: float, mnemonic: str) -> dict:
-        """
+        """ +
         Покупка токенов за DEL.
 
         :param token_address: Адрес токена.
@@ -129,8 +172,8 @@ class DecimalSDK:
         return await self.call_js_function('BuyDelForToken', token_address, str(amount), mnemonic)
 
     async def buy_del_for_token_symbol(self, symbol: str, amount: float, mnemonic: str) -> dict:
-        """
-        Покупка токенов за DEL по символу.
+        """ +
+        Покупка токенов за DEL по тикеру.
 
         :param symbol: Символ токена.
         :param amount: Сумма для покупки.
@@ -140,7 +183,7 @@ class DecimalSDK:
         return await self.call_js_function('BuyDelForTokenSymbol', symbol.lower(), str(amount), mnemonic)
 
     async def sell_del_for_token(self, token_address: str, amount: float, mnemonic: str) -> dict:
-        """
+        """ +
         Продажа токенов за DEL.
 
         :param token_address: Адрес токена.
@@ -151,8 +194,8 @@ class DecimalSDK:
         return await self.call_js_function('SellDelForToken', token_address, str(amount), mnemonic)
 
     async def sell_del_for_token_symbol(self, symbol: str, amount: float, mnemonic: str) -> dict:
-        """
-        Продажа токенов за DEL по символу.
+        """ +
+        Продажа токенов за DEL по тикеру.
 
         :param symbol: Символ токена.
         :param amount: Сумма для продажи.
@@ -173,17 +216,29 @@ class DecimalSDK:
         """
         return await self.call_js_function('ConvertTokenToToken', token_address1, token_address2, str(amount), mnemonic)
 
-    async def delegation_del(self, validator: str, days: int, amount: float, mnemonic: str) -> dict:
+    async def convert_token_to_token_symbol(self, token_symbol1: str, token_symbol2: str, amount: float, mnemonic: str) -> dict:
         """
+        Конвертация токенов по тикеру.
+
+        :param token_symbol1: Тикер первого токена.
+        :param token_symbol2: Тикер второго токена.
+        :param amount: Сумма для конвертации.
+        :param mnemonic: Мнемоническая фраза.
+        :return: Результат выполнения функции.
+        """
+        return await self.call_js_function('ConvertTokenToTokenSymbol', token_symbol1, token_symbol2, str(amount), mnemonic)
+
+    async def delegation_del(self, validator: str, days: int, amount: float, mnemonic: str) -> dict:
+        """ +
         Делегирование DEL.
 
         :param validator: Адрес валидатора.
-        :param days: Количество дней.
+        :param days: Количество дней для холда, если <= 0 то стандартное делегирование.
         :param amount: Сумма для делегирования.
         :param mnemonic: Мнемоническая фраза.
         :return: Результат выполнения функции.
         """
-        return await self.call_js_function('DelegationDEL', validator, str(days), str(amount), mnemonic)
+        return await self.call_js_function('delegationDEL', validator, int(days), str(amount), mnemonic)
 
     async def delegation_token_approve(self, validator: str, token_address: str, days: int, amount: float, mnemonic: str) -> dict:
         """
@@ -209,7 +264,22 @@ class DecimalSDK:
         :param mnemonic: Мнемоническая фраза.
         :return: Результат выполнения функции.
         """
-        return await self.call_js_function('DelegationTokenPermit', validator, token_address, str(days), str(amount), mnemonic)
+        return await self.call_js_function('DelegationToken', validator, token_address, str(days), str(amount), mnemonic)
+
+    async def delegation_token_symbol(self, validator: str, symbol: str, amount: float, days: int, mnemonic: str) -> dict:
+        """ +
+        Разрешение токена для делегирования.
+
+        :param validator: Адрес валидатора.
+        :param symbol: Тикер токена.
+        :param days: Количество дней.
+        :param amount: Сумма для разрешения.
+        :param mnemonic: Мнемоническая фраза.
+        :return: Результат выполнения функции.
+        """
+        return await self.call_js_function('DelegationTokenSymbol', validator, symbol, int(days), str(amount),
+                                           mnemonic)
+
 
 async def main():
     sdk = DecimalSDK()
